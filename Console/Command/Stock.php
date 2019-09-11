7476f98aedb5ad9d08ad017771ee11a6a0b67ee8
Dominic <dominic.watts@hotmail.co.uk>
2019-09-11 12:44:18 +0100

    base

---
+++ Console/Command/Stock.php
@@ -0,0 +1,119 @@
+<?php
+
+
+namespace Xigen\Benchmark\Console\Command;
+
+use Magento\Framework\Console\Cli;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Helper\ProgressBar;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\ConfirmationQuestion;
+
+class Stock extends Command
+{
+    const RUN_ARGUMENT = 'run';
+
+    /**
+     * @var \Psr\Log\LoggerInterface
+     */
+    protected $logger;
+    /**
+     * @var \Magento\Framework\App\State
+     */
+    protected $state;
+    /**
+     * @var \Xigen\DeleteOrder\Helper\Data
+     */
+    protected $helper;
+    /**
+     * @var \Magento\Framework\Stdlib\DateTime\DateTime
+     */
+    protected $dateTime;
+    /**
+     * @var \Symfony\Component\Console\Input\InputInterface
+     */
+    protected $input;
+    /**
+     * @var \Symfony\Component\Console\Output\OutputInterface
+     */
+    protected $output;
+
+    /**
+     * Stock constructor.
+     * @param \Psr\Log\LoggerInterface $logger
+     * @param \Magento\Framework\App\State $state
+     * @param \Xigen\AutoShipment\Helper\Shipment $shipmentHelper
+     * @param \Magento\Framework\Stdlib\DateTime\DateTime $dateTime
+     */
+    public function __construct(
+        \Psr\Log\LoggerInterface $logger,
+        \Magento\Framework\App\State $state,
+        \Xigen\Benchmark\Helper\Data $helper,
+        \Magento\Framework\Stdlib\DateTime\DateTime $dateTime
+    ) {
+        $this->logger = $logger;
+        $this->state = $state;
+        $this->helper = $helper;
+        $this->dateTime = $dateTime;
+        parent::__construct();
+    }
+
+    /**
+     * {@inheritdoc}
+     * xigen:benchmark:stock [<run>]
+     */
+    protected function execute(
+        InputInterface $input,
+        OutputInterface $output
+    ) {
+        $this->input = $input;
+        $this->output = $output;
+        $this->state->setAreaCode(\Magento\Framework\App\Area::AREA_GLOBAL);
+
+        $run = $input->getArgument(self::RUN_ARGUMENT) ?: false;
+
+        if ($run) {
+            $helper = $this->getHelper('question');
+            $question = new ConfirmationQuestion(
+                'You are about to run a benchmark test which alters stock figures on products. Are you sure?[y/N]',
+                false
+            );
+
+            if (!$helper->ask($this->input, $this->output, $question) && $this->input->isInteractive()) {
+                return Cli::RETURN_FAILURE;
+            }
+
+            $this->output->writeln((string) __('%1 Start Stock Benchmark', $this->dateTime->gmtDate()));
+
+            $skus = $this->helper->getRandomSku(100);
+  
+            $progress = new ProgressBar($this->output, count($skus));
+            $progress->start();
+
+            foreach ($skus as $sku) {
+                $this->helper->updateSkuStock($sku, $this->helper->getRandomStockNumber(), $this->output);
+                $progress->advance();
+            }
+
+            $progress->finish();
+            $this->output->writeln('');
+            $this->output->writeln((string) __('%1 Finish Stock Benchmark', $this->dateTime->gmtDate()));
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure()
+    {
+        $this->setName("xigen:benchmark:stock");
+        $this->setDescription("Stock update benchmark");
+        $this->setDefinition([
+             new InputArgument(self::RUN_ARGUMENT, InputArgument::OPTIONAL, 'Run'),
+        ]);
+        parent::configure();
+    }
+}
---
+++ Helper/Data.php
@@ -0,0 +1,189 @@
+<?php
+
+
+namespace Xigen\Benchmark\Helper;
+
+use Magento\Framework\App\Helper\AbstractHelper;
+
+class Data extends AbstractHelper
+{
+    const DEBUG = false;
+
+    /**
+     * @var \Magento\Catalog\Model\ResourceModel\Product\CollectionFactory
+     */
+    protected $productCollectionFactory;
+
+    /**
+     * @var \Magento\Catalog\Api\ProductRepositoryInterface
+     */
+    protected $productRepositoryInterface;
+
+    /**
+     * @var \Magento\CatalogInventory\Api\StockRegistryInterface
+     */
+    protected $stockRegistryInterface;
+
+    /**
+     * @var \Magento\Framework\Stdlib\DateTime\DateTime
+     */
+    protected $dateTime;
+
+    /**
+     * Data constructor.
+     * @param \Magento\Framework\App\Helper\Context $context
+     * @param \Magento\Catalog\Model\ResourceModel\Product\CollectionFactory $productCollectionFactory
+     * @param \Magento\Catalog\Api\ProductRepositoryInterface $productRepositoryInterface
+     * @param \Magento\CatalogInventory\Api\StockRegistryInterface $stockRegistryInterface
+     * @param \Magento\Framework\Stdlib\DateTime\DateTime $dateTime
+     */
+    public function __construct(
+        \Magento\Framework\App\Helper\Context $context,
+        \Magento\Catalog\Model\ResourceModel\Product\CollectionFactory $productCollectionFactory,
+        \Magento\Catalog\Api\ProductRepositoryInterface $productRepositoryInterface,
+        \Magento\CatalogInventory\Api\StockRegistryInterface $stockRegistryInterface,
+        \Magento\Framework\Stdlib\DateTime\DateTime $dateTime
+    ) {
+        $this->productCollectionFactory = $productCollectionFactory;
+        $this->productRepositoryInterface = $productRepositoryInterface;
+        $this->stockRegistryInterface = $stockRegistryInterface;
+        $this->dateTime = $dateTime;
+        parent::__construct($context);
+    }
+
+    /**
+     * Return collection of random products.
+     * @param int $limit
+     * @return \Magento\Catalog\Model\ResourceModel\Product\Collection
+     */
+    public function getRandomProduct($limit = 1)
+    {
+        $collection = $this->productCollectionFactory
+            ->create()
+            ->addAttributeToSelect('*')
+            ->addAttributeToFilter('type_id', \Magento\Catalog\Model\Product\Type::TYPE_SIMPLE)
+            ->setPageSize($limit);
+        $collection->getSelect()->order('RAND()');
+        return $collection;
+    }
+
+    /**
+     * Random stock figure - keep numbers sensible
+     * @return int
+     */
+    public function getRandomStockNumber()
+    {
+        return rand(0, 30);
+    }
+
+    /**
+     * Return array of random IDs.
+     * @param int $limit
+     * @return array
+     */
+    public function getRandomIds($limit = 1)
+    {
+        $products = $this->getRandomProduct($limit);
+        $ids = [];
+        foreach ($products as $product) {
+            $ids[] = $product->getId();
+        }
+        return $ids;
+    }
+
+    /**
+     * Return array of random SKUs.
+     * @param int $limit
+     * @return array
+     */
+    public function getRandomSku($limit = 1)
+    {
+        $products = $this->getRandomProduct($limit);
+        $skus = [];
+        foreach ($products as $product) {
+            $skus[] = $product->getSku();
+        }
+        return $skus;
+    }
+
+    /**
+     * Randomise true or false.
+     * @return bool
+     */
+    public function getRandomTrueOrFalse()
+    {
+        return (bool) rand(0, 1);
+    }
+
+    /**
+     * Get product by Id.
+     * @param $productId
+     * @param bool $editMode
+     * @param null $storeId
+     * @param bool $forceReload
+     * @return bool|\Magento\Catalog\Api\Data\ProductInterface
+     */
+    public function getProductById($productId, $editMode = false, $storeId = null, $forceReload = false)
+    {
+        try {
+            return $this->productRepositoryInterface->getById($productId, $editMode, $storeId, $forceReload);
+        } catch (\Exception $e) {
+            $this->logger->critical($e);
+            return false;
+        }
+    }
+
+    /**
+     * Get product by SKU.
+     * @param $sku
+     * @param bool $editMode
+     * @param null $storeId
+     * @param bool $forceReload
+     * @return bool|\Magento\Catalog\Api\Data\ProductInterface
+     */
+    public function getProductBySku($sku, $editMode = false, $storeId = null, $forceReload = false)
+    {
+        try {
+            return $this->productRepositoryInterface->get($sku, $editMode, $storeId, $forceReload);
+        } catch (\Exception $e) {
+            $this->logger->critical($e);
+            return false;
+        }
+    }
+
+    /**
+     * Update SKU stock
+     * @param $sku
+     * @param $qty
+     * @param null $output
+     * @return bool|void
+     * @throws \Magento\Framework\Exception\NoSuchEntityException
+     */
+    public function updateSkuStock($sku, $qty, $output = null)
+    {
+        $product = $this->getProductBySku($sku);
+        if (!$product || !$output) {
+            return;
+        }
+
+        $availability = (((string) $qty <= 0) ? '0' : '1');
+
+        $stockItem = $this->stockRegistryInterface->getStockItem($product->getId());
+        $stockItem->setData('qty', (string) $qty);
+        $stockItem->setData('is_in_stock', $availability);
+
+        try {
+            $this->stockRegistryInterface->updateStockItemBySku((string) $sku, $stockItem);
+            if (self::DEBUG) {
+                $output->writeln((string) __('%1 SKU: %2 => QTY : %3', $this->dateTime->gmtDate(), $product->getSku(), (string) $qty));
+            }
+            return true;
+        } catch (Exception $e) {
+            $this->logger->critical($e);
+            if (self::DEBUG) {
+                $output->writeln((string) __('%1 Problem SKU: %2 => QTY : %3', $this->dateTime->gmtDate(), $product->getSku(), (string) $qty));
+            }
+            return false;
+        }
+    }
+}
